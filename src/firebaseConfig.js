572b276afe7b038d2f59e79d168b642cdd165907
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-05 00:07:40 +0100

    config.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyDwUUjxK03eir6zBLrnRi90FuXl3iB1nag",
   authDomain: "twotruthsoneliegame.firebaseapp.com",
+  databaseURL:
+    "https://twotruthsoneliegame-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "twotruthsoneliegame",
   storageBucket: "twotruthsoneliegame.appspot.com",
   messagingSenderId: "47547899762",
-  appId: process.env.APP_ID,
+  appId: "1:47547899762:web:009cbb1791dd4680e9c7eb",
   measurementId: "G-0TG2XZZCV5",
 };
 
