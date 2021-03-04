42319993d35c69116570391164257a42a74ee08f
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-04 23:58:56 +0100

    quick fix

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "twotruthsoneliegame.firebaseapp.com",
+  projectId: "twotruthsoneliegame",
+  storageBucket: "twotruthsoneliegame.appspot.com",
+  messagingSenderId: "47547899762",
+  appId: process.env.APP_ID,
+  measurementId: "G-0TG2XZZCV5",
+};
+
+export default firebaseConfig;
