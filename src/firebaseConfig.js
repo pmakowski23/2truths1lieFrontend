8b1fdf7a9e5172dd8aaed568f0e77ea5f997e32a
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-07 15:46:13 +0100

    Hide api keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDwUUjxK03eir6zBLrnRi90FuXl3iB1nag",
+  apiKey: process.env.API_KEY,
   authDomain: "twotruthsoneliegame.firebaseapp.com",
   databaseURL:
     "https://twotruthsoneliegame-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "twotruthsoneliegame",
   storageBucket: "twotruthsoneliegame.appspot.com",
   messagingSenderId: "47547899762",
-  appId: "1:47547899762:web:009cbb1791dd4680e9c7eb",
+  appId: process.env.APP_ID,
   measurementId: "G-0TG2XZZCV5",
 };
 
