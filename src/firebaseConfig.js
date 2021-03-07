7cf6e5f8848512a00e8f972b82cd9604e7a00445
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-07 15:54:13 +0100

    try again with env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.REACT_APP_CUSTOM_API_KEY,
   authDomain: "twotruthsoneliegame.firebaseapp.com",
   databaseURL:
     "https://twotruthsoneliegame-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "twotruthsoneliegame",
   storageBucket: "twotruthsoneliegame.appspot.com",
   messagingSenderId: "47547899762",
-  appId: process.env.APP_ID,
+  appId: process.env.REACT_APP_CUSTOM_APP_ID,
   measurementId: "G-0TG2XZZCV5",
 };
 
